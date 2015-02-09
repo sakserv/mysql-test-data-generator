51de3240edfec8f502239e1ba1d74f90b1e94ef4
Shane Kumpf <sakserv@sakserv.org>
2015-02-09 07:27:56 -0700

    added script to install java and maven

---
+++ bin/install_maven.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+JAVA_ROOT=/usr/lib/jvm/jre-1.7.0-openjdk.x86_64
+
+# Install Java
+echo -e "\n#### Installing java-1.7.0-openjdk"
+yum install java-1.7.0-openjdk -y
+
+# Install Maven
+echo -e "\n#### Installing Maven"
+cd /tmp
+wget http://www.gtlib.gatech.edu/pub/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
+tar -xzvf apache-maven-3.2.5-bin.tar.gz
+export M2_HOME=/tmp/apache-maven-3.2.5/
+export M2=$M2_HOME/bin
+export JAVA_HOME=$JAVA_ROOT
+export PATH=$PATH:$M2:$JAVA_HOME/bin
+mvn --version
