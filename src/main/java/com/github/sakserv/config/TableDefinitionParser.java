3f0b245c8f4fa0da50075c83079c48aa20d1b4b7
Shane Kumpf <sakserv@sakserv.org>
2015-04-29 11:42:04 -0600

    parse columns from json

--- src/main/java/com/github/sakserv/config/TableDefinitionParser.java
+++ src/main/java/com/github/sakserv/config/TableDefinitionParser.java
@@ -13,10 +13,19 @@
  */
 package com.github.sakserv.config;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class TableDefinitionParser {
 
@@ -28,9 +37,48 @@ public class TableDefinitionParser {
         return jsonFileName;
     }
 
-    public void setPropFileName(String jsonFileName) {
+    public void setJsonFileName(String jsonFileName) {
         this.jsonFileName = jsonFileName;
     }
 
+    public String getCreateTableStatement() {
+        if (jsonFileName == null) {
+            LOG.error("ERROR: Must set the json file name");
+            throw new IllegalArgumentException("ERROR: Must set the json file name");
+        }
+
+        JSONParser jsonParser = new JSONParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(jsonFileName);
+        String fileContents = convertStremToString(inputStream);
+
+        try {
+            Object obj = jsonParser.parse(fileContents);
+            JSONObject jsonObject = (JSONObject) obj;
+
+            // Build up the SQL for the columns
+            Map<String,Map<String,String>> colNames = (Map<String,Map<String,String>>) jsonObject.get("columns");
+            Iterator it = colNames.entrySet().iterator();
+            StringBuilder sb = new StringBuilder();
+            while(it.hasNext()) {
+                Map.Entry pair = (Map.Entry)it.next();
+                String colName = (String) pair.getKey();
+                Map<String, String> values = ((Map<String, Map<String,String>>) jsonObject.get("columns")).get(colName);
+                String type = (String) values.get("type");
+                String qualifiers = values.get("qualifiers");
+                sb.append(colName + " " + type + " " + qualifiers + ",\n");
+            }
+            LOG.info(sb.toString());
+
+        } catch(ParseException e) {
+            LOG.error("ERROR: Could not parse JSON file " + jsonFileName);
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    static String convertStremToString(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+        return scanner.hasNext() ? scanner.next() : "";
+    }
 
 }
